149a83474e72ae392a4bce47ca5d892a77127f15
Camila Suda <contato@alyma.com.br>
2026-03-25 14:32:47 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,14 @@
-// Add your custom JavaScript for checkout here.
+// Trava de segurança no servidor para impedir criação de pedidos com scripts
+window.ecomCheckout.on('checkout', (data) => {
+  const checkMalicious = (obj) => {
+    for (const key in obj) {
+      if (typeof obj[key] === 'string' && /<script|xss|inws\.cloud|["'><]/gi.test(obj[key])) {
+        alert('Erro de segurança: Pedido bloqueado por conter caracteres inválidos.');
+        window.location.href = '/';
+        throw new Error('Tentativa de fraude bloqueada.');
+      }
+    }
+  };
+  checkMalicious(data.customer);
+  checkMalicious(data.shipping_address);
+});// Add your custom JavaScript for checkout here.
